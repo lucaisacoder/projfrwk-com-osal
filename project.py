5480bde2af2367e2d119aa50b4724508c2242c8e
lucaisacoder <lucas_hsueh@hotmail.com>
2020-01-30 23:36:59 +0800

    update py
    Change-Id: Iffe74ec660cf33285acaffaeaf3d7fded863b6b9

--- project.py
+++ project.py
@@ -5,15 +5,15 @@ sys.path.append(r'../../tools/scripts/python-libs')
 
 sdk_env_name = "MY_SDK_PATH"
 
-# get SDK absolute path
-sdk_path = os.path.abspath(sys.path[0]+"/../../")
+# get sdk absolute path
+sdk_path = os.path.abspath(sys.path[0] + "/../../")
 try:
     sdk_path = os.environ[sdk_env_name]
 except Exception:
     pass
-print("-- SDK_PATH:{}".format(sdk_path))
+print(">>> SDK_PATH:{}".format(sdk_path))
 
-# execute project script from SDK
-project_file_path = sdk_path+"/tools/scripts/project.py"
+# execute project script from sdk
+project_file_path = sdk_path + "/tools/scripts/project.py"
 with open(project_file_path) as f:
     exec(f.read())
