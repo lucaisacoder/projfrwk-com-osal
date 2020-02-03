18123839811c581b3f5ab16fe3502cf1e312b76d
lucaisacoder <lucas_hsueh@hotmail.com>
2020-02-03 23:02:55 +0800

    update Kconfig
    Change-Id: I56d0b5d246af6e7b7b677090a741f32e78277761

--- code/src/osal_api.c
+++ code/src/osal_api.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include "osal.h"
+#include "local_kconfig.h"
+#include "global_kconfig.h"
 
 int osal_api()
 {
 	printf("COM: osal, FUNCTION:%s, VERSION:%d\n", __FUNCTION__, COM_OSAL_VERSION);
+#ifdef COM_OSAL_DEFINITION
+	printf("KCONFIG: enable\n");
+#endif
 	return 0;
 }
